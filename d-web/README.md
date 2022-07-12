cae858d9b8cd0749101d5a658f85353bb028cd53
Andrew Robie <arobie1992@yahoo.com>
2022-07-12 07:29:49 -0400

    Dlang web server impl

---
+++ d-web/README.md
@@ -0,0 +1,39 @@
+executable: d-web
+size: 24MB
+Instructions:
+1. Install D: https://dlang.org/install.html
+2. cd into `d-web`
+3. Run `source ~/dlang/dmd-2.100.0/activate`
+4. Run `dub build`
+5. Run `./d-web`
+
+Request:
+curl -X POST \
+localhost:8082/call \
+-H "Content-Type: application/json" \
+-d '{"url": "http://localhost:8080/resp"}' \
+-v -w %{time_connect}:%{time_starttransfer}:%{time_total}\\n
+
+Time
+connect:transfer:total
+0.007204:0.018770:0.018828
+0.007111:0.009244:0.009299
+0.007416:0.009627:0.009698
+
+Dependencies
+vibe.d - web server; part of core language
+asdf - json serlialization/deserialization
+
+Pros
+- Pretty simple
+- Nice support for json
+- Web server library seems to be part of core language
+- Familar syntax
+- Native package manager
+
+Cons
+- Unfamiliar language
+- Not the best error messages
+- Requires package for JSON support
+- Larger executable
+- Slowest, especially first request
---
+++ d-web/source/app.d
@@ -0,0 +1,53 @@
+import vibe.vibe;
+import std.net.curl;
+import std.conv;
+import asdf;
+
+interface IRest
+{
+    struct Info 
+    {
+	    int id;
+        string status;
+	    Data data;
+    }
+
+    struct Data 
+    {
+	    string[] requestors;
+    }
+
+    @path("/call")
+    Info postReq(string url);
+}
+
+class Rest: IRest
+{
+    /*
+    Post data as:
+        { "url": "http://localhost:8080/resp" }
+    */
+    Info postReq(string url)
+    {
+        auto resp = to!string(get(url));
+        auto info = resp.deserialize!Info;
+        return info;
+    }
+}
+
+void main()
+{
+    auto router = new URLRouter;
+    router.registerRestInterface(new Rest);
+
+    auto settings = new HTTPServerSettings;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.port = 8082;
+    auto listener = listenHTTP(settings, router);
+    scope (exit)
+	{
+		listener.stopListening();
+	}
+    runApplication();
+    
+}
